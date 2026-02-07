15b3771f2c01c5fa660d3927ebf43004ba682d42
Kashish Chawla <kashishc122@gmail.com>
2026-02-07 18:16:57 +0530

    Add Privacy, Disclaimer, and Terms routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import Contact from "./pages/Contact";
 import PracticeAreas from "./pages/PracticeAreas";
 import About from "./pages/About";
 import Team from "./pages/Team";
+import Privacy from "./pages/Privacy";
+import Disclaimer from "./pages/Disclaimer";
+import Terms from "./pages/Terms";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -24,6 +27,9 @@ const App = () => (
           <Route path="/practice-areas" element={<PracticeAreas />} />
           <Route path="/about" element={<About />} />
           <Route path="/team" element={<Team />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/disclaimer" element={<Disclaimer />} />
+          <Route path="/terms" element={<Terms />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
